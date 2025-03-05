8cf71a8126727eb6d4634998426fa8aeda630d42
Evgeniy.A <andreev.e.b@mail.ru>
2025-03-05 11:56:57 +0300

    Добавлены лого для команд

--- Assets/Scripts/UI/MatchView.cs
+++ Assets/Scripts/UI/MatchView.cs
@@ -1,6 +1,7 @@
 using SimulatorEPL.Events;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace SimulatorEPL.UI
 {
@@ -9,7 +10,13 @@ namespace SimulatorEPL.UI
         [SerializeField]
         private GameCoefsView coefsView;
         [SerializeField]
-        private TextMeshProUGUI matchNameText;
+        private TextMeshProUGUI teamHomeTitle;
+        [SerializeField]
+        private Image teamHomeLogo; 
+        [SerializeField]
+        private TextMeshProUGUI teamAwayTitle;
+        [SerializeField]
+        private Image teamAwayLogo;
         [Space]
         [SerializeField]
         private TextMeshProUGUI timerText;
@@ -27,7 +34,12 @@ namespace SimulatorEPL.UI
         {
             Match = match;
 
-            matchNameText.text = match.ToString();
+            teamHomeTitle.text = match.teamHome.Title;
+            teamHomeLogo.sprite = match.teamHome.Icon;
+
+            teamAwayTitle.text = match.teamAway.Title;
+            teamAwayLogo.sprite = match.teamAway.Icon;
+
             coefsView.SetGameCoef(match.Coefs);
 
             OnMatchTimeChanged(match.CurrentTime);
